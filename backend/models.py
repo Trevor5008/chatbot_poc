eb7035178ef505964736f7572315d2d162bca55d
trevor <twern005@fiu.edu>
2024-10-23 12:28:00 -0700

    Added template code for orm

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,9 @@
-from flask import flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+from config import db
+
+## SCHEMA ##
+# table example 
+# class Contact(db.Model):
+#     id = db.Column(db.integer, primary_key=True)
+#     first_name = db.Column(db.String(80), unique=False, nullable=False)
+#     last_name = db.Column(db.String(80), unique=False, nullable=False)
+#     email = db.Column(db.String(80), unique=False, nullable=False)
